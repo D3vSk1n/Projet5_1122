3bb5d8493379cee1cb67f4ad03d70b5bbf11f1ba
Fabien Desquines <desquines.fabien@yahoo.fr>
2022-10-25 02:36:29 +0200

    essais boucle accueil mais ne fonctionne pas

--- front/js/script.js
+++ front/js/script.js
@@ -1,7 +1,3 @@
-let moi = 'gros noob';
-let salut = `bonjour ${moi}!`;
-console.log(salut);
-
 fetch("http://localhost:3000/api/products")
 .then(function(fetchAllData) {
     if (fetchAllData.ok) {
@@ -9,7 +5,13 @@ fetch("http://localhost:3000/api/products")
     }
 })
 .then(function(allDataAPI) {
-    console.log(allDataAPI);
-});
+    for (let sofa of allDataAPI) {
+        const sofaName = sofa.name;
+        const sofaDescription = sofa.description;
+        const sofaALT = sofa.altTxt;
+        const sofaImgUrl = sofa.imageUrl;
 
-console.log(allDataAPI[0].colors[1])
+        const startSection = document.getElementById('items');
+        startSection.innerHTML = "<a href="./product.html"><article><img src="`${sofaImgUrl}`" alt="`${sofaALT}`"><h3 class="productName">`${sofaName}`</h3><p class="productDescription">`${sofaDescription}`</p></article></a>";
+    }
+})
