a1da2fcf73ff7dd12093a8736b75ed8fe6ce6ea0
Fabien Desquines <desquines.fabien@yahoo.fr>
2022-11-07 16:43:22 +0100

    lien produit/panier presque fait, manque la condition

--- front/js/product.js
+++ front/js/product.js
@@ -1,5 +1,5 @@
 let actualURL = new URL(window.location.href); 
-let searchParams = new URLSearchParams(actualURL.search); //création d'une variable contenant l'URL actuel (actualURL) et activation du search params 
+let searchParams = new URLSearchParams(actualURL.search);           //création d'une variable contenant l'URL actuel (actualURL) et activation du search params 
 
 if (searchParams.has("id")) {
     let productID = searchParams.get("id");
@@ -14,7 +14,7 @@ if (searchParams.has("id")) {
             } //création du json contenant les données du fetch 
         })
         .then(function(applyData) {
-            const arrayData = [applyData]; //création d'un tableau contenant les données de la promesse
+            const arrayData = [applyData];          //création d'un tableau contenant les données de la promesse
 
             const imgSection = document.getElementsByClassName('item__img')[0];
             imgSection.innerHTML = `<img src="${arrayData[0].imageUrl}" alt="${arrayData[0].altTxt}"></img>`;
@@ -26,14 +26,14 @@ if (searchParams.has("id")) {
             priceSection.textContent = `${arrayData[0].price}`;
 
             const descriptionSection = document.getElementById('description');
-            descriptionSection.textContent = `${arrayData[0].description}`; //remplissage des quatre sections grâce aux données du tableau
+            descriptionSection.textContent = `${arrayData[0].description}`;         //remplissage des quatre sections grâce aux données du tableau
 
             for (let color of arrayData[0].colors) { 
                 let colorSection = document.getElementById('colors');
                 let optionElement = document.createElement('option');
 
                 colorSection.appendChild(optionElement);
-                optionElement.setAttribute("value", `"${color}"`);
+                optionElement.setAttribute("value", `${color}`);
                 optionElement.textContent = `${color}`;
             }
                 /*mise en place d'une boucle créant des éléments option de façon dynamique et les remplissant 
@@ -45,8 +45,36 @@ if (searchParams.has("id")) {
         })
 }
 
-const buttonAddToCart = document.getElementById('addToCart'); //récupération de l'élément qu'on va écouter, ici le bouton "ajouter au panier"
+const buttonAddToCart = document.getElementById('addToCart');       //récupération de l'élément qu'on va écouter, ici le bouton "ajouter au panier"
+const sofaID = searchParams.get("id"); 
+const sofaColor = document.querySelector("#colors");
+const sofaQuantity = document.querySelector("#quantity");           //déclaration de variables en pointant les balises contenant les informations voulues
+
+function getColorChosen() {
+    sofaColor.addEventListener('change', function(event) {
+        localStorage.setItem("color", `${event.target.value}`)
+    });
+}
+
+function getQuantityRequired() {
+    sofaQuantity.addEventListener('change', function(event) {
+        localStorage.setItem("quantity", `${event.target.value}`)
+    });
+}  
+/* déclaration de deux fonctions permettant l'écoute des champs couleurs 
+    et quantité et inscrivant la valeur choisie par l'utilisateur dans le LS */
+
+getColorChosen()
+getQuantityRequired()
 
 buttonAddToCart.addEventListener('click', function(eventInfo) {
-    window.alert("Canapé dans le panier, bien joué les Bulls !");
-});
+    let actualBuy = {
+        id: `${sofaID}`,
+        color: localStorage.color,
+        quantity: localStorage.quantity,
+    };
+    
+    let cart = [];
+    console.log(actualBuy)
+    window.alert("Canapé dans le panier, bien joué les Bulls !")
+})
